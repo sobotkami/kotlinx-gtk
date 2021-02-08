6d23960c495e5b3fde982055977dc7c72aa60c1f
doomsdayrs <doomsdayrs@gmail.com>
2021-02-08 14:31:58 -0500

    Added

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("multiplatform") version "1.4.30"
+}
+
+group = "com.github.doomsdayrs.lib"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    explicitApi()
+
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    kotlin {
+        linuxX64("native") {
+            val main by compilations.getting
+            val gtk by main.cinterops.creating
+            val x11 by main.cinterops.creating
+            binaries {
+                executable()
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting{
+            dependencies{
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
---
+++ src/commonMain/kotlin/com.github.doomsdayrs.lib.kotlinx.gtk/gtk/GtkWindowGroup.kt
@@ -0,0 +1,8 @@
+package com.github.doomsdayrs.lib.kotlinx.gtk.gtk
+
+/**
+ * kotlinx-gtk
+ * 08 / 02 / 2021
+ */
+interface GtkWindowGroup {
+}
