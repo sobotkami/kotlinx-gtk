aa58649cc9b1871ebf8fbfe7fff400b05642cc24
doomsdayrs <doomsdayrs@gmail.com>
2021-03-15 23:59:17 -0400

    Bin.kt Added functions

--- src/nativeMain/kotlin/kotlin/gtk/container/bin/Bin.kt
+++ src/nativeMain/kotlin/kotlin/gtk/container/bin/Bin.kt
@@ -1,9 +1,11 @@
 package kotlin.gtk.container.bin
 
 import gtk.GtkBin
+import gtk.gtk_bin_get_child
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.reinterpret
 import kotlin.gtk.container.Container
+import kotlin.gtk.widgets.Widget
 
 /**
  * kotlinx-gtk
@@ -12,5 +14,8 @@ import kotlin.gtk.container.Container
 open class Bin internal constructor(
 	internal val binPointer: CPointer<GtkBin>
 ) : Container(binPointer.reinterpret()) {
+
+	val child: Widget?
+		get() = gtk_bin_get_child(binPointer)?.let { Widget(it) }
 
 }
