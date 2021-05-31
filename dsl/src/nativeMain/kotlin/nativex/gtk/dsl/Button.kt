14f2090e5d90ef00c43f8e09261daa5de8c4748d
Clocks <doomsdayrs@gmail.com>
2021-05-31 13:41:07 -0600

    Added grid dsl & removed excess annotations

--- dsl/src/nativeMain/kotlin/nativex/gtk/dsl/Button.kt
+++ dsl/src/nativeMain/kotlin/nativex/gtk/dsl/Button.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.collect
 import nativex.GtkDsl
 import nativex.async.launchUnconfined
 import nativex.gtk.widgets.container.Container
+import nativex.gtk.widgets.container.Grid
 import nativex.gtk.widgets.container.bin.button.Button
 
 @GtkDsl
@@ -13,8 +14,19 @@ inline fun Container.button(
 	buttonBuilder: Button.() -> Unit = {}
 ) = Button(label).apply(buttonBuilder).also { add(it) }
 
+@GtkDsl
+inline fun Grid.button(
+	label: String,
+	x: Int,
+	y: Int,
+	width: Int,
+	height: Int,
+	buttonBuilder: Button.() -> Unit = {}
+) = Button(label).apply(buttonBuilder).also {
+	attach(it, x, y, width, height)
+}
 
-@ExperimentalCoroutinesApi
+
 @GtkDsl
 inline fun Button.onClicked(crossinline onClicked: suspend () -> Unit) {
 	launchUnconfined {
