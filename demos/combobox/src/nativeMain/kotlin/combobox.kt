dd66bd0f32987fbaef3b4effb570a4588fa88189
doomsdayrs <doomsdayrs@gmail.com>
2021-06-05 15:49:58 -0400

    Removed extra junk

--- demos/combobox/src/nativeMain/kotlin/combobox.kt
+++ demos/combobox/src/nativeMain/kotlin/combobox.kt
@@ -1,45 +1,8 @@
 import nativex.gio.dsl.onCreateUI
-import nativex.glib.KGType
-import nativex.glib.KGValue
-import nativex.gtk.ListStore
-import nativex.gtk.TreeModel
 import nativex.gtk.dsl.application
 import nativex.gtk.dsl.applicationWindow
 import nativex.gtk.dsl.comboBoxText
 import nativex.gtk.dsl.grid
-
-const val ICON_NAME_COL = 0
-const val TEXT_COL = 1
-
-fun createIconStore(): TreeModel {
-	val iconNamesToLabels = arrayOf(
-		"dialog-warning" to "Warning",
-		"process-stop" to "Stop",
-		"document-new" to "New",
-		"edit-clear" to "Clear",
-		null to null,
-		"document-open" to "Open"
-	)
-
-
-	val iter = TreeModel.TreeIter()
-	val store = ListStore(KGType.STRING, KGType.STRING)
-
-	for ((iconName, label) in iconNamesToLabels) {
-		println("doing $iconName")
-		if (iconName != null) {
-			store.append(iter)
-			store.setValue(iter, ICON_NAME_COL, KGValue(iconName))
-			store.setValue(iter, TEXT_COL, KGValue(label!!))
-		} else {
-			store.append(iter)
-			store.setValue(iter, ICON_NAME_COL, null)
-			store.setValue(iter, TEXT_COL, KGValue("separator"))
-		}
-	}
-
-	return store.asTreeModel()
-}
 
 fun main() {
 	application("com.github.doomsdayrs.test") {
