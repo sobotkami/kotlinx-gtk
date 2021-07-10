fb40bed91c7f9477c4aece53b925e4398340c33e
doomsdayrs <doomsdayrs@gmail.com>
2021-07-09 23:44:20 -0400

    Add placeholder file for node

---
+++ nativex/src/nativeMain/kotlin/nativex/gmodule/Node.kt
@@ -0,0 +1,15 @@
+package nativex.gmodule
+
+/**
+ * kotlinx-gtk
+ *
+ * 09 / 07 / 2021
+ *
+ * @see <a href=""></a>
+ */
+;
+
+/**
+ * @see <a href="https://developer.gnome.org/glib/unstable/glib-N-ary-Trees.html#GCopyFunc">GCopyFunc</a>
+ */
+//typealias CopyFunction<T> = (T) -> T
