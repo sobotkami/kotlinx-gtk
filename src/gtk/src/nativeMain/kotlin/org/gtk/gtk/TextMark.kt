8051a1305af6f3bd8118d738f566e2b029a0db1d
doomsdayrs <doomsdayrs@gmail.com>
2021-09-04 00:07:26 -0400

    Update TextMark.kt documentation

--- src/gtk/src/nativeMain/kotlin/org/gtk/gtk/TextMark.kt
+++ src/gtk/src/nativeMain/kotlin/org/gtk/gtk/TextMark.kt
@@ -14,20 +14,20 @@ import org.gtk.gtk.TextBuffer.Companion.wrap
  *
  * 17 / 07 / 2021
  *
- * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html>GtkTextMark</a>
+ * @see <a href="https://docs.gtk.org/gtk4/class.TextMark.html>GtkTextMark</a>
  */
 class TextMark(val markPointer: CPointer<GtkTextMark>) : KGObject(markPointer.reinterpret()) {
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-new">
+	 * @see <a href="https://docs.gtk.org/gtk4/ctor.TextMark.new.html">
 	 *     gtk_text_mark_new</a>
 	 */
 	constructor(leftGravity: Boolean, name: String? = null) : this(gtk_text_mark_new(name, leftGravity.gtk)!!)
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-get-visible">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.get_visible.html">
 	 *     gtk_text_mark_get_visible</a>
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-set-visible">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.set_visible.html">
 	 *     gtk_text_mark_set_visible</a>
 	 */
 	var isVisible: Boolean
@@ -35,28 +35,28 @@ class TextMark(val markPointer: CPointer<GtkTextMark>) : KGObject(markPointer.re
 		set(value) = gtk_text_mark_set_visible(markPointer, value.gtk)
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-get-deleted">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.get_deleted.html">
 	 *     gtk_text_mark_get_deleted</a>
 	 */
 	val isDeleted: Boolean
 		get() = gtk_text_mark_get_deleted(markPointer).bool
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-get-name">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.get_name.html">
 	 *     gtk_text_mark_get_name</a>
 	 */
 	val name: String?
 		get() = gtk_text_mark_get_name(markPointer)?.toKString()
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-get-buffer">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.get_buffer.html">
 	 *     gtk_text_mark_get_buffer</a>
 	 */
 	val buffer: TextBuffer?
 		get() = gtk_text_mark_get_buffer(markPointer).wrap()
 
 	/**
-	 * @see <a href="https://developer.gnome.org/gtk3/stable/GtkTextMark.html#gtk-text-mark-get-left-gravity">
+	 * @see <a href="https://docs.gtk.org/gtk4/method.TextMark.get_left_gravity.html">
 	 *     gtk_text_mark_get_left_gravity</a>
 	 */
 	val leftGravity: Boolean
